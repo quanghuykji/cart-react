3946c17548bb67fb7efba084a7be82385e4defa0
quanghuykji <quanghuykji@gmail.com>
2024-11-03 22:07:04 +0700

    first commit

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 
-const api_auth='http://localhost:8080/web-vnsim/auth';
-const api='http://localhost:8080/web-vnsim';
+const api_auth='https://deploy-kz1k.onrender.com/web-vnsim/auth';
+const api='https://deploy-kz1k.onrender.com/web-vnsim';
 
 
 export {api_auth,api};
